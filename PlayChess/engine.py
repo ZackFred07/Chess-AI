97c31430e9a22ff3352fc8b40d19a4b39d1bb39e
Zackary Kiener <64226837+ZackFred07@users.noreply.github.com>
2023-03-28 13:03:43 -0400

    Zack's Untested Stability Function 👉🏻👈🏻

--- PlayChess/engine.py
+++ PlayChess/engine.py
@@ -1,9 +1,16 @@
 import random
+
+from chess import BaseBoard, Piece, piece_symbol, square
+import chess
+
+import symbol
 # Milo is trash
 # Quinn best chess player of all time
 
 global abPruning
 global count
+
+
 def evaluate_position(board, fen):
     """ Evaluates the position of the board based on material
     :param fen:
@@ -32,6 +39,7 @@ def evaluate_position(board, fen):
 
     return evaluation
 
+
 def find_depth_move(board, depth):
     global abPruning
     global count
@@ -41,6 +49,7 @@ def find_depth_move(board, depth):
     ans = find_depth_move_recursion(board, 0, depth)
     print(count)
     return ans
+
 
 def find_depth_move_recursion(board, depth, returnDepth):
     """ Checks position evaluation out of all possibilities depth moves forward and chooses best move
@@ -60,19 +69,21 @@ def find_depth_move_recursion(board, depth, returnDepth):
         if not abPruning[depth]:
             abPruning[depth] = evaluate_position(board, board.fen())
         else:
-            abPruning[depth] = min(abPruning[depth], evaluate_position(board, board.fen()))
+            abPruning[depth] = min(
+                abPruning[depth], evaluate_position(board, board.fen()))
     else:
         if depth != 0:
             # White just moved, favor positive values
             if not abPruning[depth]:
                 abPruning[depth] = evaluate_position(board, board.fen())
             else:
-                abPruning[depth] = max(abPruning[depth], evaluate_position(board, board.fen()))
+                abPruning[depth] = max(
+                    abPruning[depth], evaluate_position(board, board.fen()))
 
     searchTree = True
     if depth != 0:
         if abPruning[depth] != evaluate_position(board, board.fen()):
-            searchTree = False # If you want to test without alpha-beta pruning, set this to True!
+            searchTree = False  # If you want to test without alpha-beta pruning, set this to True!
 
     if depth == returnDepth or not searchTree:
         return evaluate_position(board, board.fen())
@@ -89,7 +100,8 @@ def find_depth_move_recursion(board, depth, returnDepth):
         board.pop()
 
     for i in range(-50, 50, 1):
-        if not vs[i]: vs.pop(i)
+        if not vs[i]:
+            vs.pop(i)
 
     if depth == 0:
         if board.turn:
@@ -106,7 +118,9 @@ def find_depth_move_recursion(board, depth, returnDepth):
 # New engine must always evaluate at least two moves ahead
 # New engine must always evaluate positions where this function returns 1 (UNLESS PAST 10 DEPTH)
 # Overall depth will vary from 2-10 but be much quicker to calculate
-def not_stable(board):
+
+
+def not_stable(board: BaseBoard, color: chess.Color):
     """
     Returns 1 if not quasi stable
         - If any piece is attacked by another of lower value
@@ -117,21 +131,26 @@ def not_stable(board):
     :param board:
     :return: 1 | 0
     """
-    # Edge cases (need to be dealt with externally i.e: display game result and such)
-    # if board.is_check() or board.is_stalemate() or board.is_fivefold_repetition() or board.is_seventyfive_moves() or board.is_insufficient_material() or board.is_checkmate()
-        # return 1
-    # loop through every spot on the board:
-        # if it contains a piece:
-            # loop through board.attackers(chess.WHITE, square) and board.attackers(chess.BLACK, square) and add them to different saved values
-            # if these values are not equal
-                # return 1
-            # else if these values are not 0
-                # if any of the attackers is less valuable than attacked piece
-                    # return 1
-        # else
-            # if there is a check from this square
-                # if this square is controlled by an opponent piece
-                    # return 1
-    # end loop
-    # return 0
+    valuations = {'K': 100, 'Q': 9, 'R': 5, 'N': 3, 'B': 3, 'P': 1,  # White
+                  'k': -100, 'q': -9, 'r': -5, 'n': -3, 'b': -3, 'p': -1,  # Black
+                  }
 
+    # Edge cases (need to be dealt with externally i.e: display game result and such)
+    if board.is_check() or board.is_stalemate() or board.is_fivefold_repetition() or board.is_seventyfive_moves() or board.is_insufficient_material() or board.is_checkmate():
+        return 1
+    for square in board.pieces(color=color):
+        attackers = (valuations[Piece.symbol(board.piece_type_at(sq))]
+                     for sq in board.attackers(not color, square))
+        defenders = (valuations[Piece.symbol(board.piece_type_at(sq))]
+                     for sq in board.attackers(color, square))
+        if len(attackers) > len(defenders):
+            return 1
+        if len(attackers) < len(defenders):
+            return 0
+        if len(attackers) == len(defenders):
+            if attackers[0] > valuations[Piece.symbol(board.piece_type_at(square))]:
+                return 1
+            for i in range(1, attackers):
+                if (attackers[i] > defenders[i-1]):
+                    return 1
+    return 0
